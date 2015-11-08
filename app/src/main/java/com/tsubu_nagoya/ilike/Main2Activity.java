13afbca5a6f347663d9d0197955f5cb8b4de80fa
Ryota Morita <ryota.morita.3.8@gmail.com>
2015-11-08 15:40:33 +0900

    Complated FB login

--- app/src/main/java/com/tsubu_nagoya/ilike/Main2Activity.java
+++ app/src/main/java/com/tsubu_nagoya/ilike/Main2Activity.java
@@ -1,8 +1,17 @@
 package com.tsubu_nagoya.ilike;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.tsubu_nagoya.ilike.ilike.R;
 
 import com.facebook.FacebookSdk;
@@ -12,22 +21,20 @@ public class Main2Activity extends ActionBarActivity {
 
     CallbackManager callbackManager;
 
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater,
-            ViewGroup container,
-            Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.splash, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        loginButton = (LoginButton) view.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
-        // If using in a fragment
-        loginButton.setFragment(this);
-        // Other app specific specialization
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
-        // Callback registration
+
+        setContentView(R.layout.activity_main2);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
+                        @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
             }
@@ -42,20 +49,6 @@ public class Main2Activity extends ActionBarActivity {
                 // App code
             }
         });
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-
-
-        setContentView(R.layout.activity_main2);
-
-        callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) view.findViewById(R.id.usersettings_fragment_login_button);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() { ... });
 
     }
 
