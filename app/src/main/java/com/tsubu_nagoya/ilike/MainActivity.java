76c4709ec52658e52b9fe005c4199ed1bb5411da
SawakiYuta <yuta199511@gmail.com>
2015-11-08 14:35:42 +0900

    38 query error

--- app/src/main/java/com/tsubu_nagoya/ilike/MainActivity.java
+++ app/src/main/java/com/tsubu_nagoya/ilike/MainActivity.java
@@ -1,7 +1,11 @@
 package com.tsubu_nagoya.ilike;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.GridView;
 
@@ -17,14 +21,29 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Bitmap> pictureList = load();
-    }
-    BitmapAdapter adapter = new BitmapAdapter(
-            getApplicationContext(), R.layout.list_picture, pictureList);
+        BitmapAdapter adapter = new BitmapAdapter(
+                getApplicationContext(), R.layout.list_picture, pictureList);
 
-    GridView gridView = (GridView)findViewById(R.id.gridView1);
-    gridView.setAdapter(adapter);
+        GridView gridView = (GridView) findViewById(R.id.gridView1);
+        gridView.setAdapter(adapter);
+    }
 
     private ArrayList<Bitmap> load() {
+        ArrayList<Bitmap> list = new ArrayList<Bitmap>();
+        ContentResolver cr = getContentResolver();
+        Uri uri = MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+        ContentResolver resolver = new ContentResolver(getApplicationContext()) {
 
+        };
+        Cursor c = resolver.query(uri, null, null, null, null);
+  //      Cursor c = managedQuery(uri, null, null, null, null);
+        c.moveToFirst();
+        for (int i = 0; i < c.getCount(); i++) {
+            long id = c.getLong(c.getColumnIndexOrThrow("_id"));
+            Bitmap bmp = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MICRO_KIND, null);
+            list.add(bmp);
+            c.moveToNext();
+        }
+        return list;
     }
 }
