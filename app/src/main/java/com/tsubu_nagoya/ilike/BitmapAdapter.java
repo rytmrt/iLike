a50c7345d9edae46d7d15c9307abf620dd78dc61
SawakiYuta <yuta199511@gmail.com>
2015-11-08 13:12:38 +0900

    make branch

---
+++ app/src/main/java/com/tsubu_nagoya/ilike/BitmapAdapter.java
@@ -0,0 +1,7 @@
+package com.tsubu_nagoya.ilike;
+
+/**
+ * Created by Yuta on 2015/11/08.
+ */
+public class BitmapAdapter {
+}
--- app/src/main/java/com/tsubu_nagoya/ilike/MainActivity.java
+++ app/src/main/java/com/tsubu_nagoya/ilike/MainActivity.java
@@ -1,9 +1,13 @@
 package com.tsubu_nagoya.ilike;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.GridView;
 
 import com.tsubu_nagoya.ilike.ilike.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +15,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        ArrayList<Bitmap> pictureList = load();
+    }
+    BitmapAdapter adapter = new BitmapAdapter(
+            getApplicationContext(), R.layout.list_picture, pictureList);
+
+    GridView gridView = (GridView)findViewById(R.id.gridView1);
+    gridView.setAdapter(adapter);
+
+    private ArrayList<Bitmap> load() {
+
+    }
 }
